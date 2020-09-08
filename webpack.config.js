70f5f917f08e03cd1cc32849d518f4f7f06d7d6b
Why7090 <whyharry9@gmail.com>
2020-09-08 09:56:14 -0400

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const SizePlugin = require('size-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
@@ -8,24 +7,15 @@ module.exports = {
 	stats: 'errors-only',
 	entry: {
 		background: './source/background',
-		options: './source/options'
+		content: './source/content',
+		common: './source/common'
 	},
 	output: {
 		path: path.join(__dirname, 'distribution'),
 		filename: '[name].js'
 	},
 	plugins: [
-		new SizePlugin(),
-		new CopyWebpackPlugin([
-			{
-				from: '**/*',
-				context: 'source',
-				ignore: ['*.js']
-			},
-			{
-				from: 'node_modules/webextension-polyfill/dist/browser-polyfill.min.js'
-			}
-		])
+		new SizePlugin()
 	],
 	optimization: {
 		minimizer: [
